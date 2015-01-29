7c84c1d42a5238dc6e58c1bad461328b5843f813
Talon Cloud <Austin_Sandlin@baylor.edu>
2015-01-28 21:23:40 -0600

    Makefile + Demo
    Added a makefile for the current file structure.
    Added a main.cpp that has OpenGL structure for the demo.

--- Project/src/Common/Events/EventHandlerMap.cpp
+++ Project/src/Common/Events/EventHandlerMap.cpp
@@ -1,9 +1,9 @@
 #include "EventHandlerMap.h"
 
 EventHandlerMap::~EventHandlerMap() {
-	for ( auto it = handlers->begin(); it != handlers->end(); it++ ) {
-		for ( auto it2 = it->second->begin(); it2 != it->second->end(); it2++ ) {
-			delete it2;
+	for ( auto it = handlers.begin(); it != handlers.end(); it++ ) {
+		for ( auto it2 = it->second.begin(); it2 != it->second.end(); it2++ ) {
+			delete *it2;
 		}
 	}
 }
@@ -18,8 +18,8 @@ void EventHandlerMap::AddEventHandler( StringID id, EventHandler * handler ) {
 void EventHandlerMap::RemoveEventHandler( StringID id ) {
 	auto it = handlers.find( id );
 	if ( it != handlers.end() ) {
-		for ( auto it2 = it->second->begin(); it2 != it->second->end(); it2++ ) {
-			delete it2;
+		for ( auto it2 = it->second.begin(); it2 != it->second.end(); it2++ ) {
+			delete *it2;
 		}
 	}
 }
@@ -28,7 +28,7 @@ void EventHandlerMap::HandleEvent( Event * event ) {
 	auto id = event->GetID();
 	auto it = handlers.find( id );
 	if ( it != handlers.end() ) {
-		for ( auto it2 = it->second->begin(); it2 != it->second->end(); it2++ ) {
+		for ( auto it2 = it->second.begin(); it2 != it->second.end(); it2++ ) {
 			it2->HandleEvent( event );
 		}
 	}
