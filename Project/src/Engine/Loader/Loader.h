d536d585804d3b27eef7ec953f43a3f72ed6e336
Isaac F Fisher <Isaac_Fisher@baylor.edu>
2015-03-03 01:12:33 -0600

    Removed unneeded libraries

--- Project/src/Engine/Loader/Loader.h
+++ Project/src/Engine/Loader/Loader.h
@@ -4,30 +4,23 @@
 #include <fstream>
 #include "../../Common/Resources/Constants.h"
 
+
 class Loader {
+
   private:
   	fstream reader;
-
-
-
-
-
-
-
-
-
-
-
-
-
+    static AudioController& audio_controller;
+    static InputController& input_controller;
+    static ObjectController& object_controller;
+    static RenderController& render_controller;
+    static GameController& game_controller;
 
 
   public:
+  	loader();
+  	~loader();
 
-
-
-
-
+  	void loadLevel(const string filename);
 
 };
 
