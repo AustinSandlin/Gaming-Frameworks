34a2f5530fb5c6c247f7ced8489f5f4cc27a31d4
Isaac F Fisher <Isaac_Fisher@baylor.edu>
2015-03-03 00:12:09 -0600

    Mostly-empty loader files

--- Project/src/Common/Resources/Constants.h
+++ Project/src/Common/Resources/Constants.h
@@ -1,7 +1,9 @@
 #ifndef _CONSTANTS_H
 #define _CONSTANTS_H
 
+#include <string>
 
+const string LOADER_MAGIC_STRING = "level";
 
 
 
--- Project/src/Engine/Loader/Loader.h
+++ Project/src/Engine/Loader/Loader.h
@@ -1,9 +1,12 @@
 #ifndef _LOADER_H
 #define _LOADER_H
 
+#include <fstream>
 #include "../../Common/Resources/Constants.h"
 
 class Loader {
+  private:
+  	fstream reader;
 
 
 
@@ -19,9 +22,7 @@ class Loader {
 
 
 
-
-
-
+  public:
 
 
 
